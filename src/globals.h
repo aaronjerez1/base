a5948b31a74a41d63262c1cacd1671bf80bb9c60
aaronjerez1 <aaronjerez1@gmail.com>
2025-03-30 15:04:01 -0400

    added CsCript serialize fucntions

--- src/globals.h
+++ src/globals.h
@@ -87,9 +87,79 @@ namespace Globals {
 
 	class CTxIn
 	{
+	public:
 		COutPoint prevout;
 		CScript scriptSig;
-	public:
+		unsigned int nSequence;
+
+
+		IMPLEMENT_SERIALIZE(
+			READWRITE(prevout);
+			READWRITE(scriptSig);
+			READWRITE(nSequence);)
+
+		CTxIn()
+		{
+			nSequence = UINT_MAX;
+		}
+		
+		explicit CTxIn(COutPoint preoutIn, CScript scriptSigIn = CScript(), unsigned int nSequence = UINT_MAX)
+		{
+			prevout = preoutIn;
+			scriptSig = scriptSigIn;
+			nSequence = nSequence;
+		}
+
+		CTxIn(uint256 hashPrevTx, unsigned int nOut, CScript scriptSigIn = CScript(), unsigned int nSequenceIn = UINT_MAX)
+		{
+			prevout = COutPoint(hashPrevTx, nOut);
+			scriptSig = scriptSigIn;
+			nSequence = nSequenceIn;
+		}
+
+		bool IsFinal() const
+		{
+			return (nSequence == UINT_MAX);
+		}
+
+
+		friend bool operator==(const CTxIn& a, const CTxIn& b)
+		{
+			return (a.prevout == b.prevout &&
+				a.scriptSig == b.scriptSig &&
+				a.nSequence == b.nSequence);
+		}
+
+
+		friend bool operator!=(const CTxIn& a, const CTxIn& b)
+		{
+			return !(a == b);
+		}
+
+		std::string ToString() const
+		{
+			std::string str;
+			str += strprintf("CTxIn(");
+			str += prevout.ToString();
+			if (prevout.IsNull())
+				str += strprintf(", coinbase %s", HexStr(scriptSig.begin(), scriptSig.end(), false).c_str());
+			else
+				str += strprintf(", scriptSig=%s", scriptSig.ToString().substr(0, 24).c_str());
+			if (nSequence != UINT_MAX)
+				str += strprintf(", nSequence=%u", nSequence);
+			str += ")";
+			return str;
+		}
+		void print() const
+		{
+			printf("%s\n", ToString().c_str());
+		}
+		
+		///
+
+		//bool IsMine() const;
+		//int64 GetDebit() const; TODO: 
+
 	};
 
 
--- src/shared/serialize.h
+++ src/shared/serialize.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <bits/stdc++.h>
+#include "../script/script.h"
 #include "types.h"
 
 //
@@ -338,6 +339,25 @@ void Unserialize(Stream& is, std::set<K, Pred, A>& m, int nType, int nVersion)
     }
 }
 
+//
+// others derived from vector
+//
+inline unsigned int GetSerializeSize(const CScript& v, int nType, int nVersion)
+{
+    return GetSerializeSize((const std::vector<unsigned char>&)v, nType, nVersion);
+}
+
+template<typename Stream>
+void Serialize(Stream& os, const CScript& v, int nType, int nVersion)
+{
+    Serialize(os, (const std::vector<unsigned char>&)v, nType, nVersion);
+}
+
+template<typename Stream>
+void Unserialize(Stream& is, CScript& v, int nType, int nVersion)
+{
+    Unserialize(is, (std::vector<unsigned char>&)v, nType, nVersion);
+}
 
 
 
